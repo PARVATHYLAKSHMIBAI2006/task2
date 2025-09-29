b285c4dda65382b13f23f3009c0fef101ad21ca5
B00997912 <Lakshmibai-P@ulster.ac.uk>
2025-09-29 16:56:23 +0100

    Add project files.

---
+++ week1task2/Program.cs
@@ -0,0 +1,10 @@
+﻿void task1()
+{
+    Console.WriteLine("PLEASE ENTER YOUR NAME");
+    string name = Console.ReadLine();
+    Console.WriteLine("PLEASE ENTER YOUR AGE");
+    int age = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine($"HELLO {name}, YOU ARE {age} YEARS OLD");
+}
+task1();
