696845f4a1c190b813ece7b2947695f291d4f54e
B00997912 <Lakshmibai-P@ulster.ac.uk>
2025-09-29 17:10:56 +0100

    task2

--- week1task2/Program.cs
+++ week1task2/Program.cs
@@ -1,10 +1,27 @@
-﻿void task1()
+﻿void task2()
 {
-    Console.WriteLine("PLEASE ENTER YOUR NAME");
-    string name = Console.ReadLine();
+    
     Console.WriteLine("PLEASE ENTER YOUR AGE");
     int age = Convert.ToInt32(Console.ReadLine());
+    if ( age<13)
+    {
+     
+        Console.WriteLine("You are a child");
+    }
+    else if (age>=13 && age<20)
+    {
+        Console.WriteLine("You are a teenager");
+    }
+    else if (age>=20 && age<65)
+    {
+        Console.WriteLine("You are an adult");
+    }
+    else
+    {
+        Console.WriteLine("You are a senior citizen");
+    }
 
-    Console.WriteLine($"HELLO {name}, YOU ARE {age} YEARS OLD");
+
+
 }
-task1();
+task2();
